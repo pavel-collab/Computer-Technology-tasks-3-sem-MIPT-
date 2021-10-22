11743f17624cad0d1aea42acd107bba816f1f0eb
pavel <rubikpf2002@mail.ru>
2021-10-23 00:28:57 +0300

    feat(task 13.1)

--- 13.1_crproc/main.c
+++ 13.1_crproc/main.c
@@ -5,7 +5,7 @@
 
 // получаем информацию о процессе
 void proc_info(const char* procname) {
-    printf("%s: PID %d, PPID %d, PGID %d, SID %d\n", procname, getpid(), getppid(), getpgid(0), getsid(0));
+    printf("[%s]:\nPID %d,\nPPID %d,\nPGID %d,\nSID %d\n\n", procname, getpid(), getppid(), getpgid(0), getsid(0));
 }
 
 int main(int argc, char *argv[]) {
@@ -20,14 +20,15 @@ int main(int argc, char *argv[]) {
     }
 
     if (cpid == 0) {            /* Code executed by child */
-        printf("Child PID is %ld\n", (long) getpid());
+        proc_info("Child");
         if (argc == 1)
             pause();            /* Wait for signals */
-        _exit(atoi(argv[1]));   // завердиться с кодом, который передали в аргументе 1
+        _exit(atoi(argv[1]));   // завершится с кодом, который передали в аргументе 1
 
     } 
     else {                      /* Code executed by parent */
 
+        proc_info("Parent");
         do {
             w = waitpid(cpid, &wstatus, WUNTRACED | WCONTINUED);
             if (w == -1) {
@@ -37,17 +38,48 @@ int main(int argc, char *argv[]) {
 
             // waitpid -- процесс родитель дожидается ИЗМЕНЕНИЯ статуса процесса потомка
             // далее, в зависимости от статуса процесса потомка, программа выводит сообщение об это статусе
+            // --------------------------------------------------------------------------------------------
             if (WIFEXITED(wstatus)) {
-                printf("exited, status=%d\n", WEXITSTATUS(wstatus));
+                printf("the child process has been completed\nstatus = %d\n", WEXITSTATUS(wstatus));
             } else if (WIFSIGNALED(wstatus)) {
-                printf("killed by signal %d\n", WTERMSIG(wstatus));
+                printf("the child process has been killed by signal %d\n", WTERMSIG(wstatus));
             } else if (WIFSTOPPED(wstatus)) {
-                printf("stopped by signal %d\n", WSTOPSIG(wstatus));
+                printf("the child process has been stopped by signal %d\n", WSTOPSIG(wstatus));
             } else if (WIFCONTINUED(wstatus)) {
                 printf("continued\n");
             }
+
+            #ifdef WCOREDUMP
+            if (WCOREDUMP(wstatus)) {
+                printf("child proc has cause a core dump\n");
+            }
+            #endif  
         } while (!WIFEXITED(wstatus) && !WIFSIGNALED(wstatus));
+        // крутимся в цикле, пока дочерний процесс не завершиться (успешно или по сигналу)
 
         exit(EXIT_SUCCESS);
     }
 }
+
+/*
+
+WIFEXITED(wstatus)   -- не равно нулю, если дочерний процесс успешно завершился
+
+WIFSIGNALED(wstatus) -- возвращает истинное значение, если дочерний процесс завершился из-за необработанного сигнала
+
+WIFSTOPPED(wstatus)  -- возвращает истинное значение, если дочерний процесс, 
+                        из-за которого функция вернула управление, в настоящий момент остановлен; 
+                        это возможно, только если использовался флаг WUNTRACED
+
+*/
+
+// ./out 0 -> the child process has been completed 
+//            status = 0
+
+// ./out -> ^Z -> ps -> kill -18 <cpid> -> the child process has been stopped by signal 20 
+//                                         continued
+//                                         child proc has cause a core dump
+
+// ./out -> ^Z -> ps -> kill -11 <cpid> -> the child process has been killed by signal 11
+//                                         child proc has cause a core dump
+
