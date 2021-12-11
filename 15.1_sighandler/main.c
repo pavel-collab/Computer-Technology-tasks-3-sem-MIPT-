f366b7f738b296e536793556de6d285538695157
Pavel Filippenko <rubikpf2002@mail.ru>
2021-12-11 21:50:09 +0300

    15.1 main and version2 signal handler

--- 15.1_sighandler/main.c
+++ 15.1_sighandler/main.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <signal.h>
 
-//? говорим компилятору, что поведение этой переменной нельзя оптимизировать (volatile)
+// поведение этой переменной нельзя оптимизировать
 volatile int g_last_signal;
 
 void sig_handler(int signum) {
@@ -11,25 +11,36 @@ void sig_handler(int signum) {
 
 int main() {
 
-    // сигналы 9 и 18 нельзя перехватить
-    //TODO: сделать обработку ошибок
-    // -_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
-    //TODO: какой флаг надо указать, чтобы выводилась дополнительная информация (от кого пришел сигнал)
-    signal(SIGINT, sig_handler); // ^C
-    signal(SIGQUIT, sig_handler); // ^/
-    signal(SIGTSTP, sig_handler); //? ^Z 
-    signal(SIGHUP, sig_handler); // обрыв соединения
-    signal(SIGTERM, sig_handler); // завершение работы
+    // чтобы убить прогу выполнить в параллельном терминале kill -9 <pid>
+    // чтобы посмотреть pid процесса в параллельном терминале ps ux
+
+    //! сигналы 9 и 18 нельзя перехватить
+    if (signal(SIGINT, sig_handler) == SIG_ERR){
+        perror("signal(SIGINT)");
+        return -1;
+    } // ^c
+    if (signal(SIGQUIT, sig_handler)) {
+        perror("signal(SIGQUIT)");
+        return -1;
+    } // ^/
+    if (signal(SIGTSTP, sig_handler)) {
+        perror("signal(SIGTSTP)");
+        return -1;
+    } // ^Z 
+    if (signal(SIGHUP, sig_handler)) {
+        perror("signal(SIGHUP)");
+        return -1;
+    } // обрыв соединения
+    if (signal(SIGTERM, sig_handler)) {
+        perror("signal(SIGTERM)");
+        return -1;
+    } // завершение работы
     
+    printf("to discover process pid, in another console use:\n \tps ux\n");
+    printf("to kill programm, in another console use:\n \tkill -9 <pid>\n\n");
     while(1) {
         pause();
         printf("\tSignal %d came\n", g_last_signal);
     }
     return 0;
 }
-
-
-// man 2 kill
-//! man 2 signal 
-// man 2 sigaction -> flags: SA_RESTART, SA_SIGINFO
-// man 2 pause
--- 15.1_sighandler/version2.c
+++ 15.1_sighandler/version2.c
@@ -13,23 +13,23 @@ volatile int proc_pid;
 //! пихать printf в обработчик плохо, потому что printf не является асинхронно-безопасной
 // простой обработчик
 void handler(int signum) {
-    // printf("\tGot signal [%d]\n", signum);
     cought_signum = signum;
 }
 
 // прокачанный обработчик
 void sig_handler(int signum, siginfo_t *info, void *ctx) {
-    // printf("\tGot signal [%d] from proces [%d]\n", signum, info->si_pid);
     cought_signum = signum;
     proc_pid = info->si_pid;
 }
 
 int main() {
 
-
     //! для простого обработчика надо заполнять поле sa_handler структуры sigaction
     //! для прокачанного обработчика нужно заполнять поле sa_sigaction
     //! man sigaction
+
+    //* чтобы убить прогу выполнить в параллельном терминале kill -9 <pid>
+    //* чтобы посмотреть pid процесса в параллельном терминале ps ux
 
     #ifdef SIMPLE_HANDLER
         struct sigaction int_handler = {0};
@@ -38,19 +38,43 @@ int main() {
         int_handler.sa_handler = handler;
         int_handler.sa_flags = SA_RESTART;
         sigaction(SIGINT, &int_handler, NULL); // SIGINT -> ^c
+    #else
+        struct sigaction term_handler = {0};
+
+        term_handler.sa_sigaction = sig_handler;
+        term_handler.sa_flags = SA_RESTART | SA_SIGINFO;
+        if (sigaction(SIGTSTP, &term_handler, NULL) == -1) {
+            perror("sigaction(SIGTSTP)");
+            return -1;
+        } // ^z
+        if (sigaction(SIGINT, &term_handler, NULL) == -1) {
+            perror("sigaction(SIGTSTP)");
+            return -1;
+        } // ^c
+        if (sigaction(SIGQUIT, &term_handler, NULL) == -1) {
+            perror("sigaction(SIGTSTP)");
+            return -1;
+        } // ^/
+        if (sigaction(SIGHUP, &term_handler, NULL) == -1) {
+            perror("sigaction(SIGTSTP)");
+            return -1;
+        } // обрыв соединения
+        if (sigaction(SIGTERM, &term_handler, NULL) == -1) {
+            perror("sigaction(SIGTSTP)");
+            return -1;
+        } // завершение работы
     #endif
 
-    //TODO дописать обработку остальных сигналов
-    struct sigaction term_handler = {0};
-
-    term_handler.sa_sigaction = sig_handler;
-    term_handler.sa_flags = SA_RESTART | SA_SIGINFO;
-    sigaction(SIGTSTP, &term_handler, NULL); // SIGTSTP -> ^z
-
+    printf("to discover process pid, in another console use:\n \tps ux\n");
+    printf("to kill programm, in another console use:\n \tkill -9 <pid>\n\n");
     while(1) {
         pause(); // wait until signum cought and processed
-        // printf("\tGot signal [%d]\n", cought_signum);
-        printf("\tGot signal [%d] from proces [%d]\n", cought_signum, proc_pid);
+
+        #ifdef SIMPLE_HANDLER
+            printf("\tGot signal [%d]\n", cought_signum);
+        #else
+            printf("\tGot signal [%d] from proces [%d]\n", cought_signum, proc_pid);
+        #endif
     }
 
     return 0;
