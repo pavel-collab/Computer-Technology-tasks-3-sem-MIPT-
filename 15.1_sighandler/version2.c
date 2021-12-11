264c620b8a9a36beba3e2827bd902bc1726c887c
Pavel Filippenko <rubikpf2002@mail.ru>
2021-12-11 16:08:47 +0300

    fix sigaction version2

--- 15.1_sighandler/version2.c
+++ 15.1_sighandler/version2.c
@@ -2,38 +2,47 @@
 #include <stdio.h>
 #include <signal.h>
 #include <string.h>
+#include <sched.h>
+
 
 //TODO look up the example from the man 2 sigaction
 //! there are some warnings
 
+// простой обработчик
 void handler(int signum) {
     printf("\tGot signal [%d]\n", signum);
 }
 
+// прокачанный обработчик
 void sig_handler(int signum, siginfo_t *info, void *ctx) {
-    printf("\tGot signal [%d] from poces [%d]\n", signum, info->si_pid);
+    printf("\tGot signal [%d] from proces [%d]\n", signum, info->si_pid);
 }
 
 int main() {
 
-    struct sigaction int_handler;
+
+    //! для простого обработчика надо заполнять поле sa_handler структуры sigaction
+    //! для прокачанного обработчика нужно заполнять поле sa_sigaction
+    //! man sigaction
+
+    struct sigaction int_handler = {0};
     // функция заполняет буффуер, на который ссылается указатель, нулями
-    memset(&int_handler, 0, sizeof(int_handler));
+    // memset(&int_handler, 0, sizeof(int_handler));
 
     int_handler.sa_handler = handler;
     int_handler.sa_flags = SA_RESTART;
-    sigaction(SIGINT, &int_handler, NULL);
+    sigaction(SIGINT, &int_handler, NULL); // SIGINT -> ^c
 
     //TODO дописать обработку остальных сигналов
-    struct sigaction term_handler;
-    memset(&term_handler, 0, sizeof(term_handler));
+    struct sigaction term_handler = {0};
+    // memset(&term_handler, 0, sizeof(term_handler));
 
-    term_handler.sa_handler = sig_handler;
+    term_handler.sa_sigaction = sig_handler;
     term_handler.sa_flags = SA_RESTART | SA_SIGINFO;
-    sigaction(SIGTERM, &term_handler, NULL);
+    sigaction(SIGTSTP, &term_handler, NULL); // SIGTSTP -> ^z
 
     while(1) {
-        sched_yield(); // man 3 sched_yield
+        sched_yield();
     }
 
     return 0;
